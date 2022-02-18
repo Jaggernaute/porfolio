83cd58dab3017985d529228d77fbb717acfa9e78
sigmanificient <edhyjox@gmail.com>
2022-02-18 19:55:20 +0100

    :lipstick: Base css

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,25 @@
 
 <script></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+:root {
+  --color-bg: #373e48;
+  --color-text: #f6faff;
+  --color-elment: var(--color-text);
+}
+
+body {
+  margin: 0;
+
+  color: var(--color-text);
+  background: var(--color-bg);
+
+  font-family: "Poppins", sans-serif;
+  font-size: 14px;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
